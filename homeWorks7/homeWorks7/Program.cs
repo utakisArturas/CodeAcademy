b61ad69fe7654ab1b18c08f4b8403464e14c03ca
utakisArturas <utakis.arturas.github@gmail.com>
2022-01-05 17:38:55 +0200

    home works
    home works

---
+++ homeWorks7/homeWorks7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace homeWorks7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter grades for 5 students : ");
+            var studentGrades = new double[5];
+
+            for (int i = 0; i < studentGrades.Length; i++)
+            {
+                studentGrades[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            foreach (var student in studentGrades)
+            {
+                Console.Write(student + " ");
+            }
+            Console.ReadLine();
+            //* biggest grade ->
+            var bigGrade = studentGrades[0];
+            foreach (var grade in studentGrades)
+            {
+                if (grade>bigGrade)
+                {
+                    bigGrade = grade;
+                }
+            }
+            Console.WriteLine($"Biggest grade in group : {bigGrade}");
+
+            //* lowest grade ->
+            var lowGrade = studentGrades[0];
+            foreach (var grade in studentGrades)
+            {
+                if (grade < lowGrade)
+                {
+                    lowGrade = grade;
+                }
+            }
+            Console.WriteLine($"Lowest grade in group : {lowGrade}");
+
+            //*sum grade ->
+            double sumGrade = 0;
+            foreach (var grade in studentGrades)
+            {
+                sumGrade += grade;
+            }
+            var avgGrade = sumGrade / studentGrades.Length;
+            Console.WriteLine($"Average grade is : {avgGrade}");
+        }
+    }
+}
